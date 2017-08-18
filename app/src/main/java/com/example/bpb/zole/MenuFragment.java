c35c5dc18b849ee87a121e6c36a5ea7f6e6a1d35
Benjamin Below <Benjamin.Below@softwire.com>
2017-08-18 17:06:05 +0100

    Add empty menu fragment

---
+++ app/src/main/java/com/example/bpb/zole/MenuFragment.java
@@ -0,0 +1,21 @@
+package com.example.bpb.zole;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MenuFragment extends Fragment {
+
+    public static MenuFragment newInstance() {
+        return new MenuFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_menu, container, false);
+    }
+}
